f227b1667c67f0c90b33f17e7739fba6fed4bf4f
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-08 21:02:18 +0900

    Update Maximize MainWindow

--- Bishojo_Game_Launcher/MainWindow.xaml.cs
+++ Bishojo_Game_Launcher/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,22 +14,24 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WindowChromeSample
-{
+namespace Bishojo_Game_Launcher {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        private string state = "Normal";
+
+        public string State {
+            get { return state; }
+            set { state = value; }
+		}
+
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void window_StateChanged(object sender, EventArgs e)
-        {
-            switch (WindowState)
-            {
+        private void window_StateChanged(object sender, EventArgs e) {
+            switch (WindowState) {
                 case WindowState.Maximized:
                     LayoutRoot.Margin = new Thickness(9);
                     break;
@@ -42,7 +45,19 @@ namespace WindowChromeSample
         }
 
         private void WindowClose_Maxmize(object sender, RoutedEventArgs e) {
-            this.WindowState = WindowState.Maximized;
+            switch (this.State) {
+                case "Normal":
+                    this.Width = SystemParameters.WorkArea.Width;
+                    this.Height = SystemParameters.WorkArea.Height;
+                    this.Left = 0;
+                    this.Top = 0;
+                    this.State = "Max";
+                    break;
+                case "Max":
+                    this.WindowState = WindowState.Normal;
+                    this.State = " Normal";
+                    break;
+            }
         }
 
         private void AddGane_Click(object sender, RoutedEventArgs e) {
