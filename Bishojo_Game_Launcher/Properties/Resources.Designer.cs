8bdf18327b98387e6bda1bbcc4f4be124e15a2de
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-08 23:04:18 +0900

    Add Tooltip to MainWindow.xaml

--- Bishojo_Game_Launcher/Properties/Resources.Designer.cs
+++ Bishojo_Game_Launcher/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Bishojo_Game_Launcher.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Bishojo_Game_Launcher.Properties {
         ///   このクラスで使用されているキャッシュされた ResourceManager インスタンスを返します。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Bishojo_Game_Launcher.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,39 @@ namespace Bishojo_Game_Launcher.Properties {
         ///   現在のスレッドの CurrentUICulture プロパティをオーバーライドします。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Close に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string WindowCloseToolTip {
+            get {
+                return ResourceManager.GetString("WindowCloseToolTip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Maxmize に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string WindowMaxmizeToolTip {
+            get {
+                return ResourceManager.GetString("WindowMaxmizeToolTip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Minimize に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string WindowMinimizeToolTip {
+            get {
+                return ResourceManager.GetString("WindowMinimizeToolTip", resourceCulture);
             }
         }
     }
