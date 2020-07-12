b8e0b88f27faef983aac21e4de0b75183f5a33f7
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-12 23:55:36 +0900

    Add Folder class and Path class in property

---
+++ Bishojo_Game_Launcher/Property/Folder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bishojo_Game_Launcher.Property {
+	class Folder {
+		public static void ExistsAllAppFolder() {
+			if (!Directory.Exists(Path.AppFolder)) {
+				CreateAppFolder();
+			}
+			if (!Directory.Exists(Path.GamesFolder)) {
+				CreateGamesFolder();
+			}
+		}
+
+		public static void CreateAppFolder() {
+			Directory.CreateDirectory(Path.AppFolder);
+		}
+
+		public static void CreateGamesFolder() {
+			Directory.CreateDirectory(Path.GamesFolder);
+		}
+
+		public static void CreateGameFolder(string gameTitleMD5) {
+			Directory.CreateDirectory(Path.GamesFolder + gameTitleMD5 + @"\");
+		}
+	}
+}
---
+++ Bishojo_Game_Launcher/Property/Path.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bishojo_Game_Launcher.Property {
+	static class Path {
+		public static string AppFolder {
+			get { return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Bishojo Game Launcher\"; }
+		}
+
+		public static string GamesFolder {
+			get { return AppFolder + @"games\"; }
+		}
+	}
+}
