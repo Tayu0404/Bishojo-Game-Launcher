3af5472aad70eca6115d7c423aae69ecfc1bec7b
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-18 12:09:48 +0900

    Update the game list UI to be richer

--- Bishojo_Game_Launcher/Windows/MainWindowGameList.xaml.cs
+++ Bishojo_Game_Launcher/Windows/MainWindowGameList.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Interop;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bishojo_Game_Launcher.Windows {
 	/// <summary>
@@ -30,11 +22,16 @@ namespace Bishojo_Game_Launcher.Windows {
 			var list = new Game.List();
 			list.Read();
 			gamelist = list.Games;
-			Console.WriteLine(gamelist[0].Detaile.Title);
-			Console.WriteLine("Test");
-			gamelist.Select(x =>
-				GameList.Items.Add(x.Detaile.Title)
-			);
+			foreach (var game in gamelist) {
+				GameList.Items.Add(new {
+					Hash = game.Hash,
+					AppIcon = Imaging.CreateBitmapSourceFromHIcon(
+						Icon.ExtractAssociatedIcon(game.ExecutableFile).Handle,
+						Int32Rect.Empty,
+						BitmapSizeOptions.FromEmptyOptions()
+					),
+					Title = game.Detaile.Title });
+			}
 			GameList.SelectedItem = 0;
 		}
 	}
