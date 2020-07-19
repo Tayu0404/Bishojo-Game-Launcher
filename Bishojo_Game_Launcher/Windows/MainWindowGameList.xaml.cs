6fbdb9eb6397f7414b11711ba404a3e3ba6b4ded
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-19 22:16:50 +0900

    Add window startup position

--- Bishojo_Game_Launcher/Windows/MainWindowGameList.xaml.cs
+++ Bishojo_Game_Launcher/Windows/MainWindowGameList.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Bishojo_Game_Launcher.Property;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
@@ -10,11 +13,24 @@ namespace Bishojo_Game_Launcher.Windows {
 	/// MainWindowGameList.xaml の相互作用ロジック
 	/// </summary>
 	public partial class MainWindowGameList : UserControl {
-		private static List<Game.List.GameDetaile> gamelist;
+		private static List<Game.Game.GameDetaile> gamelist;
 
 		public MainWindowGameList() {
 			InitializeComponent();
 			GameListReload();
+		}
+		private class ListItem {
+			public ListItem(string hash, BitmapSource appIcon, string title) {
+				this.Hash = hash;
+				this.AppIcon = appIcon;
+				this.Title = title;
+			}	
+
+			public string Hash { get; private set; }
+
+			public BitmapSource AppIcon { get; private set; }
+
+			public string Title { get; private set; }
 		}
 
 		public void GameListReload() {
@@ -23,16 +39,32 @@ namespace Bishojo_Game_Launcher.Windows {
 			list.Read();
 			gamelist = list.Games;
 			foreach (var game in gamelist) {
-				GameList.Items.Add(new {
-					Hash = game.Hash,
-					AppIcon = Imaging.CreateBitmapSourceFromHIcon(
-						Icon.ExtractAssociatedIcon(game.ExecutableFile).Handle,
-						Int32Rect.Empty,
-						BitmapSizeOptions.FromEmptyOptions()
-					),
-					Title = game.Detaile.Title });
+				GameList.Items.Add(
+					new ListItem(
+						game.Hash,
+						Imaging.CreateBitmapSourceFromHIcon(
+							Icon.ExtractAssociatedIcon(game.ExecutableFile).Handle,
+							Int32Rect.Empty,
+							BitmapSizeOptions.FromEmptyOptions()
+						),
+						game.Detaile.Title
+					)
+				);
 			}
-			GameList.SelectedItem = 0;
+			GameList.SelectedIndex = 0;
+		}
+
+		private void GameList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			var listBox = sender as ListBox;
+			var selectedItem = listBox.Items[listBox.SelectedIndex] as ListItem;
+			var detaile = gamelist.Find(x => x.Hash == selectedItem.Hash);
+			MainImage.Source = new BitmapImage(
+				new Uri(
+					AppPath.GamesFolder +
+					detaile.Hash + @"\" +
+					detaile.Hash + ".jpg"
+				)
+			);
 		}
 	}
 }
