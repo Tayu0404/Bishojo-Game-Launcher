a46459c4437b27c2df774851216bfb8616ed4701
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-19 22:14:45 +0900

    Add type of folder existence confirmation

--- Bishojo_Game_Launcher/Property/Folder.cs
+++ Bishojo_Game_Launcher/Property/Folder.cs
@@ -8,24 +8,44 @@ using System.Threading.Tasks;
 namespace Bishojo_Game_Launcher.Property {
 	class Folder {
 		public static void ExistsAllAppFolder() {
-			if (!Directory.Exists(Path.AppFolder)) {
+			if (!Directory.Exists(AppPath.AppFolder)) {
 				CreateAppFolder();
 			}
-			if (!Directory.Exists(Path.GamesFolder)) {
+			if (!Directory.Exists(AppPath.GamesFolder)) {
 				CreateGamesFolder();
 			}
 		}
 
 		public static void CreateAppFolder() {
-			Directory.CreateDirectory(Path.AppFolder);
+			Directory.CreateDirectory(AppPath.AppFolder);
 		}
 
 		public static void CreateGamesFolder() {
-			Directory.CreateDirectory(Path.GamesFolder);
+			Directory.CreateDirectory(AppPath.GamesFolder);
 		}
 
 		public static void CreateGameFolder(string gameTitleMD5) {
-			Directory.CreateDirectory(Path.GamesFolder + gameTitleMD5 + @"\");
+			var gameFolderPath = AppPath.GamesFolder + gameTitleMD5 + @"\";
+			Directory.CreateDirectory(gameFolderPath);
+			Directory.CreateDirectory(gameFolderPath + @"sample\");
+		}
+
+		public static void ExistsAppFolder() {
+			if (!Directory.Exists(AppPath.AppFolder)) {
+				CreateAppFolder();
+			}
+		}
+
+		public static void ExistsGamesFolder() {
+			if (!Directory.Exists(AppPath.GamesFolder)) {
+				CreateGamesFolder();
+			}
+		}
+
+		public static void ExistsGameFolder(string gameTitleMD5) {
+			if (!Directory.Exists(AppPath.GamesFolder + gameTitleMD5 + @"\")) {
+				CreateGameFolder(gameTitleMD5);
+			}
 		}
 	}
 }
