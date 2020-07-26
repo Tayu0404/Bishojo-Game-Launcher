d41d3f7df8951783515e6e5b15cfbfafca0f9ffa
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-26 19:20:23 +0900

    Add copyright to AssemblyInfo

--- Bishojo_Game_Launcher/Properties/AssemblyInfo.cs
+++ Bishojo_Game_Launcher/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bishoujo_Game_Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Tayu 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
