f8cf6e375a8696649e2ed8367913169a29152de9
Tayu0404 <tkt.yusei0404@gmail.com>
2020-07-30 20:53:18 +0900

    Update ToggleSwitch to support Solid Color Brush

--- Bishojo_Game_Launcher/Control/ToggleSwitch.xaml.cs
+++ Bishojo_Game_Launcher/Control/ToggleSwitch.xaml.cs
@@ -16,10 +16,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BishojoGameLauncher.Control {
-	/// <summary>
-	/// ToggleSwitch.xaml の相互作用ロジック
-	/// </summary>
-	public partial class ToggleSwitch : UserControl {
+    /// <summary>
+    /// ToggleSwitch.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ToggleSwitch : UserControl {
         public ToggleSwitch() {
             InitializeComponent();
             borderTrack.Background = new SolidColorBrush(Colors.Gray); ;
@@ -31,29 +31,29 @@ namespace BishojoGameLauncher.Control {
 
         #region ******************************* TrackBackgroundOnColor
         [Category("Switch")]
-        public Color TrackBackgroundOnColor {
-            get { return (Color)this.GetValue(TrackBackgroundOnColorProperty); }
+        public SolidColorBrush TrackBackgroundOnColor {
+            get { return (SolidColorBrush)this.GetValue(TrackBackgroundOnColorProperty); }
             set { this.SetValue(TrackBackgroundOnColorProperty, value); }
         }
 
         public static readonly DependencyProperty TrackBackgroundOnColorProperty =
-            DependencyProperty.Register("TrackBackgroundOnColor", typeof(Color),
+            DependencyProperty.Register("TrackBackgroundOnColor", typeof(SolidColorBrush),
                 typeof(ToggleSwitch),
-                new FrameworkPropertyMetadata(Colors.DeepSkyBlue,
+                new FrameworkPropertyMetadata(new SolidColorBrush(Colors.DeepSkyBlue),
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                     TrackBackgroundOnColorChangeFunc,
                     TrackBackgroundOnColorCoerceFunc));
 
         static void TrackBackgroundOnColorChangeFunc(DependencyObject target,
             DependencyPropertyChangedEventArgs e) {
-            var of = (Color)e.OldValue;
-            var nf = (Color)e.NewValue;
+            var of = (SolidColorBrush)e.OldValue;
+            var nf = (SolidColorBrush)e.NewValue;
             var obj = (ToggleSwitch)target;
         }
 
         static object TrackBackgroundOnColorCoerceFunc(DependencyObject target, object baseValue) {
             var obj = (ToggleSwitch)target;
-            var val = (Color)baseValue;
+            var val = (SolidColorBrush)baseValue;
 
             return val;
         }
@@ -61,71 +61,71 @@ namespace BishojoGameLauncher.Control {
 
         #region ******************************* TrackBackgroundOffColor
         [Category("Switch")]
-        public Color TrackBackgroundOffColor {
-            get { return (Color)this.GetValue(TrackBackgroundOffColorProperty); }
+        public SolidColorBrush TrackBackgroundOffColor {
+            get { return (SolidColorBrush)this.GetValue(TrackBackgroundOffColorProperty); }
             set { this.SetValue(TrackBackgroundOffColorProperty, value); }
         }
 
         public static readonly DependencyProperty TrackBackgroundOffColorProperty =
-            DependencyProperty.Register("TrackBackgroundOffColor", typeof(Color),
+            DependencyProperty.Register("TrackBackgroundOffColor", typeof(SolidColorBrush),
                 typeof(ToggleSwitch),
-                new FrameworkPropertyMetadata(Colors.Gray,
+                new FrameworkPropertyMetadata(new SolidColorBrush(Colors.Gray),
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                     TrackBackgroundOffColorChangeFunc));
 
         static void TrackBackgroundOffColorChangeFunc(DependencyObject target,
             DependencyPropertyChangedEventArgs e) {
-            var of = (Color)e.OldValue;
-            var nf = (Color)e.NewValue;
+            var of = (SolidColorBrush)e.OldValue;
+            var nf = (SolidColorBrush)e.NewValue;
             var obj = (ToggleSwitch)target;
-            obj.borderTrack.Background = new SolidColorBrush(nf);
+            obj.borderTrack.Background = nf;
         }
         #endregion
 
         #region ******************************* CircleBackgroundColor
         [Category("Switch")]
-        public Color CircleBackgroundColor {
-            get { return (Color)this.GetValue(CircleBackgroundColorProperty); }
+        public SolidColorBrush CircleBackgroundColor {
+            get { return (SolidColorBrush)this.GetValue(CircleBackgroundColorProperty); }
             set { this.SetValue(CircleBackgroundColorProperty, value); }
         }
 
         public static readonly DependencyProperty CircleBackgroundColorProperty =
-            DependencyProperty.Register("CircleBackgroundColor", typeof(Color),
+            DependencyProperty.Register("CircleBackgroundColor", typeof(SolidColorBrush),
                 typeof(ToggleSwitch),
-                new FrameworkPropertyMetadata(Colors.SteelBlue,
+                new FrameworkPropertyMetadata(new SolidColorBrush(Colors.SteelBlue),
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                     CircleBackgroundColorChangeFunc));
 
         static void CircleBackgroundColorChangeFunc(DependencyObject target,
             DependencyPropertyChangedEventArgs e) {
-            var of = (Color)e.OldValue;
-            var nf = (Color)e.NewValue;
+            var of = (SolidColorBrush)e.OldValue;
+            var nf = (SolidColorBrush)e.NewValue;
             var obj = (ToggleSwitch)target;
-            obj.ellipseToggle.Fill = new SolidColorBrush(nf);
+            obj.ellipseToggle.Fill = nf;
         }
         #endregion
 
         #region ******************************* CircleBorderColor
         [Category("Switch")]
-        public Color CircleBorderColor {
-            get { return (Color)this.GetValue(CircleBorderColorProperty); }
+        public SolidColorBrush CircleBorderColor {
+            get { return (SolidColorBrush)this.GetValue(CircleBorderColorProperty); }
             set { this.SetValue(CircleBorderColorProperty, value); }
         }
 
         public static readonly DependencyProperty CircleBorderColorProperty =
-            DependencyProperty.Register("CircleBorderColor", typeof(Color),
+            DependencyProperty.Register("CircleBorderColor", typeof(SolidColorBrush),
                 typeof(ToggleSwitch),
-                new FrameworkPropertyMetadata(Colors.White,
+                new FrameworkPropertyMetadata(new SolidColorBrush(Colors.White),
                     FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                     CircleBorderColorChangeFunc));
 
         static void CircleBorderColorChangeFunc(DependencyObject target,
             DependencyPropertyChangedEventArgs e) {
-            var of = (Color)e.OldValue;
-            var nf = (Color)e.NewValue;
+            var of = (SolidColorBrush)e.OldValue;
+            var nf = (SolidColorBrush)e.NewValue;
             var obj = (ToggleSwitch)target;
 
-            obj.ellipseToggle.Stroke = new SolidColorBrush(nf);
+            obj.ellipseToggle.Stroke = nf;
         }
         #endregion
 
@@ -151,14 +151,14 @@ namespace BishojoGameLauncher.Control {
             var obj = (ToggleSwitch)target;
 
             if (nf) {
-                obj.borderTrack.Background = new SolidColorBrush(obj.TrackBackgroundOffColor);
-                var ca = new ColorAnimation(obj.TrackBackgroundOnColor, TimeSpan.FromSeconds(obj.Duration));
+                obj.borderTrack.Background = new SolidColorBrush(obj.TrackBackgroundOffColor.Color);
+                var ca = new ColorAnimation(obj.TrackBackgroundOnColor.Color, TimeSpan.FromSeconds(obj.Duration));
                 obj.borderTrack.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
                 var da = new DoubleAnimation(10, TimeSpan.FromSeconds(obj.Duration));
                 obj.translateTransform.BeginAnimation(TranslateTransform.XProperty, da);
             } else {
-                obj.borderTrack.Background = new SolidColorBrush(obj.TrackBackgroundOnColor);
-                var ca = new ColorAnimation(obj.TrackBackgroundOffColor, TimeSpan.FromSeconds(obj.Duration));
+                obj.borderTrack.Background = new SolidColorBrush(obj.TrackBackgroundOnColor.Color);
+                var ca = new ColorAnimation(obj.TrackBackgroundOffColor.Color, TimeSpan.FromSeconds(obj.Duration));
                 obj.borderTrack.Background.BeginAnimation(SolidColorBrush.ColorProperty, ca);
                 var da = new DoubleAnimation(-10, TimeSpan.FromSeconds(obj.Duration));
                 obj.translateTransform.BeginAnimation(TranslateTransform.XProperty, da);
