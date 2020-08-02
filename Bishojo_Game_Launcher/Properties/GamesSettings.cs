c8c62135e36269f28834f864bc7a9d2bd28313b3
Tayu0404 <tkt.yusei0404@gmail.com>
2020-08-02 20:50:16 +0900

    Add GamesSetting

---
+++ Bishojo_Game_Launcher/Properties/GamesSettings.cs
@@ -0,0 +1,22 @@
+﻿using BishojoGameLauncher.Configuration;
+using BishojoGameLauncher.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishojoGameLauncher.Properties {
+	[SettingsPath(DirectoryName = @".\conf\", FileName = "Games.conf")]
+	[DataContract]
+	public class GamesSettings : SettingsBase<GamesSettings> {
+		public static readonly GamesSettings Instance = Load();
+
+		[DataMember]
+		public Dictionary<string, GameDetaile> Games {
+			get { return Get(a => a.Games); }
+			set { Set(a => a.Games, value);}
+		}
+	}
+}
