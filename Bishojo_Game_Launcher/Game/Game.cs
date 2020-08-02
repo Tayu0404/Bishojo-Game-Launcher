ee0831837ffae22ddeb01972035c1e6564fd6828
Tayu0404 <tkt.yusei0404@gmail.com>
2020-08-02 20:02:00 +0900

    Organize Game Class

--- Bishojo_Game_Launcher/Game/Game.cs
+++ Bishojo_Game_Launcher/Game/Game.cs
@@ -1,5 +1,4 @@
-﻿using BishojoGameLauncher.Property;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,108 +6,11 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace BishojoGameLauncher.Game {
-	class Game {
-        public class SearchResult {
-            public SearchResult(
-                string title,
-                string brand,
-                string sellday,
-                string detaileirl
-            ) {
-                this.Title = title;
-                this.Brand = brand;
-                this.Sellday = sellday;
-                this.Detaileurl = detaileirl;
-            }
-            public string Title { get; private set; }
-
-            public string Brand { get; private set; }
-
-            public string Sellday { get; private set; }
-
-            public string Detaileurl { get; private set; }
-        }
-
-        public class Detaile {
-            public Detaile(
-                string title,
-                string web,
-                string brand,
-                string sellday,
-                string mainImage,
-                string erogame,
-                List<string> illustrators,
-                List<string> scenarios,
-                List<string> composers,
-                List<string> voices,
-                List<string> characters,
-                List<string> singers,
-                List<string> sampleCGs
-            ) {
-                this.Title = title;
-                this.Web = web;
-                this.Brand = brand;
-                this.Sellday = sellday;
-                this.MainImage = mainImage;
-                this.Erogame = erogame;
-                this.Illustrators = illustrators;
-                this.Scenarios = scenarios;
-                this.Composers = composers;
-                this.Voices = voices;
-                this.Characters = characters;
-                this.Singers = singers;
-                this.SampleCGs = sampleCGs;
-            }
-
-            public string Title { get; private set; }
-
-            public string Web { get; private set; }
-
-            public string Brand { get; private set; }
-
-            public string Sellday { get; private set; }
-
-            public string MainImage { get; private set; }
-
-            public string Erogame { get; private set; }
-
-            public List<string> Illustrators { get; private set; }
-
-            public List<string> Scenarios { get; private set; }
-
-            public List<string> Composers { get; private set; }
-
-            public List<string> Voices { get; private set; }
-
-            public List<string> Characters { get; private set; }
-
-            public List<string> Singers { get; private set; }
-
-            public List<string> SampleCGs { get; private set; }
-        }
-
-        public class GameDetaile {
-            public GameDetaile(string hash, string executableFile, string saveFolder, Game.Detaile detaile, bool downloadComplete = false) {
-                this.Hash = hash;
-                this.ExecutableFile = executableFile;
-                this.SaveFolder = saveFolder;
-                this.DownloadComplete = downloadComplete;
-                this.Detaile = detaile;
-            }
-            public string Hash { get; private set; }
-
-            public string ExecutableFile { get; private set; }
-
-            public string SaveFolder { get; private set; }
-
-            public bool DownloadComplete { get; set; }
-
-            public Detaile Detaile { get; private set; }
-        }
-
-        public static string GenerateHash(string gameTitle) {
+	class Generate {
+        public static string Hash(string gameTitle) {
             MD5 md5 = MD5.Create();
             var gameTitleBytes = Encoding.UTF8.GetBytes(gameTitle);
             var gameTitleMD5Bytes = md5.ComputeHash(gameTitleBytes);
@@ -119,5 +21,124 @@ namespace BishojoGameLauncher.Game {
             }
             return stringBuilder.ToString();
         }
+    }
+
+    class SearchResult {
+        public SearchResult(
+            string title,
+            string brand,
+            string sellday,
+            string detaileirl
+        ) {
+            this.Title = title;
+            this.Brand = brand;
+            this.Sellday = sellday;
+            this.Detaileurl = detaileirl;
+        }
+        public string Title { get; private set; }
+
+        public string Brand { get; private set; }
+
+        public string Sellday { get; private set; }
+
+        public string Detaileurl { get; private set; }
+    }
+
+    [DataContract]
+    public class Detaile {
+        public Detaile(
+            string title,
+            string web,
+            string brand,
+            string sellday,
+            string mainImage,
+            string erogame,
+            List<string> illustrators,
+            List<string> scenarios,
+            List<string> composers,
+            List<string> voices,
+            List<string> characters,
+            List<string> singers,
+            List<string> sampleCGs
+        ) {
+            this.Title = title;
+            this.Web = web;
+            this.Brand = brand;
+            this.Sellday = sellday;
+            this.MainImage = mainImage;
+            this.Erogame = erogame;
+            this.Illustrators = illustrators;
+            this.Scenarios = scenarios;
+            this.Composers = composers;
+            this.Voices = voices;
+            this.Characters = characters;
+            this.Singers = singers;
+            this.SampleCGs = sampleCGs;
+        }
+
+        [DataMember]
+        public string Title { get; private set; }
+
+        [DataMember]
+        public string Web { get; private set; }
+
+        [DataMember]
+        public string Brand { get; private set; }
+
+        [DataMember]
+        public string Sellday { get; private set; }
+
+        [DataMember]
+        public string MainImage { get; private set; }
+
+        [DataMember]
+        public string Erogame { get; private set; }
+
+        [DataMember]
+        public List<string> Illustrators { get; private set; }
+
+        [DataMember]
+        public List<string> Scenarios { get; private set; }
+
+        [DataMember]
+        public List<string> Composers { get; private set; }
+
+        [DataMember]
+        public List<string> Voices { get; private set; }
+
+        [DataMember]
+        public List<string> Characters { get; private set; }
+
+        [DataMember]
+        public List<string> Singers { get; private set; }
+
+        [DataMember]
+        public List<string> SampleCGs { get; private set; }
+    }
+
+    [DataContract]
+    public class GameDetaile {
+        public GameDetaile(string hash, string executableFile, string saveFolder, Detaile detaile, bool downloadComplete = false) {
+            this.Hash = hash;
+            this.ExecutableFile = executableFile;
+            this.SaveFolder = saveFolder;
+            this.DownloadComplete = downloadComplete;
+            this.Detaile = detaile;
+        }
+        
+        [DataMember]
+        public string Hash { get; private set; }
+
+        [DataMember]
+        public string ExecutableFile { get; private set; }
+
+        [DataMember]
+        public string SaveFolder { get; private set; }
+
+        [DataMember]
+        public bool DownloadComplete { get; set; }
+
+        [DataMember]
+        public Detaile Detaile { get; private set; }
     }
 }
