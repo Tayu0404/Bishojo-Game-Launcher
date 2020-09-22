3297ce769d864f616767ffb1b77a78bfa8fdc977
Tayu0404 <tkt.yusei0404@gmail.com>
2020-09-22 18:22:35 +0900

    Add Search Class

---
+++ Bishojo_Game_Launcher/Database/Search.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishojoGameLauncher.Database {
+	class Search {
+		public static GameData.GamesRow Hash(GameData gameData, string target) {
+			var result = gameData.Games.AsEnumerable().Where(r => r.Hash == target).ToArray();
+			return result[0];
+		}
+	}
+}
