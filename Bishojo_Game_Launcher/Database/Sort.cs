b30f6c49e13670c0ece2c29fc24a06c31de472f3
Tayu0404 <tkt.yusei0404@gmail.com>
2020-09-22 12:44:52 +0900

    Move Sort from Properties to Database

---
+++ Bishojo_Game_Launcher/Database/Sort.cs
@@ -0,0 +1,42 @@
+﻿using BishojoGameLauncher.Game;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishojoGameLauncher.Database {
+	class Sort {
+
+		public static List<GameData.GamesRow> TitleAscendingOrder(GameData gameData){
+			var result = gameData.Games.AsEnumerable().OrderBy(r => r.Table).ToList();
+			return result;
+		}
+
+		public static List<GameData.GamesRow> TitleDescendingOrder(GameData gameData) {
+			var result = gameData.Games.AsEnumerable().OrderByDescending(r => r.Table).ToList();
+			return result;
+		}
+
+		public static List<GameData.GamesRow> BrandAscendingOrder(GameData gameData) {
+			var result = gameData.Games.AsEnumerable().OrderBy(r => r.Brand).ToList();
+			return result;
+		}
+
+		public static List<GameData.GamesRow> BrandDescendingOrder(GameData gameData) {
+			var result = gameData.Games.AsEnumerable().OrderByDescending(r => r.Brand).ToList();
+			return result;
+		}
+
+		public static List<GameData.GamesRow> SellDayAscendingOrder(GameData gameData) {
+			var result = gameData.Games.AsEnumerable().OrderBy(r => r.Sellday).ToList();
+			return result;
+		}
+
+		public static List<GameData.GamesRow> SellDayDescendingOrder(GameData gameData) {
+			var result = gameData.Games.AsEnumerable().OrderByDescending(r => r.Sellday).ToList();
+			return result;
+		}
+	}
+}
