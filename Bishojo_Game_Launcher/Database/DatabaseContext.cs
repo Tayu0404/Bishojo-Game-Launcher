f0b407b4a9b909236a9f24f3b020688519c3d542
Tayu0404 <tkt.yusei0404@gmail.com>
2020-09-22 12:51:41 +0900

    Add Database Context

---
+++ Bishojo_Game_Launcher/Database/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishojoGameLauncher.Database {
+	public class DatabaseContext {
+		
+		public DatabaseContext() {
+			Database = new GameData();
+			try {
+				Read();
+			} catch {
+				
+			}
+		}
+
+		private GameData _database;
+
+		public GameData Database {
+			get { return _database; }
+			private set { _database = value; }
+		}
+
+		public string FilePath {
+			get { return $"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\Games.db"; }
+		}
+
+		public void Save() {
+			try {
+				Database.WriteXml(FilePath);
+			}
+			catch {
+				throw;
+			}
+		}
+
+		public void Read() {
+			try {
+				Database.ReadXml(FilePath);
+			}
+			catch {
+				throw;
+			}
+		}
+	}
+}
